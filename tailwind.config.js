8db5ede55c022d5c8a4d93d74bceba683da50113
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-06 09:53:10 +0300

    update title and add tailwind external property

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#E31C1C",
+        secondary: "#32475C",
+      },
+    },
   },
   plugins: [],
-}
+};
