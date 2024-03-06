4bc4ce631400e846f3c190cc10752baeb3fbd980
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-06 11:01:38 +0300

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
-export default function App() {
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Four0Four from "./pages/Four0Four";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
+import Products from "./pages/Products";
+import Service from "./pages/Service";
+import About from "./pages/About";
+import Navbar from "./components/layout/Navbar";
+import Copy from "./components/layout/Copy";
+import Footer from "./components/layout/Footer";
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="*" element={<Four0Four />} />
+      </Routes>
+      <Footer />
+      <Copy />
+    </div>
+  );
+};
+
+export default App;
