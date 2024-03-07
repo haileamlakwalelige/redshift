a6fd84b222821af811982a5d72a7bd98b9d3e3a9
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-07 11:37:46 +0300

    update hoe page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import HomeIntro from '../components/home/HomeIntro'
 import HomeRed from '../components/home/HomeRed'
+import HomeAbout from "../components/home/HomeAbout";
+import HomeService from "../components/home/HomeService";
+import HomeProducts from "../components/home/HomeProducts";
+import Scroll from "../components/Reusable/Scroll";
+import Button from "../components/Reusable/Button";
+import Button2 from "../components/Reusable/Button2";
+import HomeTestimony from "../components/home/HomeTestimony";
 
 const Home = () => {
   return (
-    <div className='mt-32'>
+    <div className="mt-32 overflow-x-hidden">
       <HomeIntro />
       <HomeRed />
+      <HomeAbout />
+      <HomeService />
+      <HomeProducts />
+      <Scroll />
+      <Button />
+      {/* <Button2 /> */}
+      <HomeTestimony />
     </div>
-  )
-}
+  );
+};
 
 export default Home
