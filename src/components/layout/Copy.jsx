3b2ce199d548ae913a8f1bb2a2fe1a62ca86a16a
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-08 11:02:14 +0300

    update copyright

--- src/components/layout/Copy.jsx
+++ src/components/layout/Copy.jsx
@@ -5,7 +5,7 @@ const Copy = () => {
   const currentYear = new Date().getFullYear();
   return (
     <div className="bg-primary ">
-      <div className="flex justify-center md:justify-between items-center lg:gap-10 flex-wrap mx-2 sm:mx-6 py-2">
+      <div className="flex justify-center md:justify-between items-center lg:gap-10 flex-wrap mx-2 sm:mx-6 py-3">
         <div className="flex justify-center items-center gap-2 text-white ">
           <FaRegCopyright size={30} />
           <p>{currentYear} Redshift. All right reserved.</p>
