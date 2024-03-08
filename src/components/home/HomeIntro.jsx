83e4c1816b2ff2930901f2a6e637bf62c3656c58
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-08 09:28:40 +0300

    update home intro

--- src/components/home/HomeIntro.jsx
+++ src/components/home/HomeIntro.jsx
@@ -14,10 +14,10 @@ const HomeIntro = () => {
       }}
       className="bg-[#F6FDFE] relative lg:bg-[url(${contact})] lg:bg-cover flex justify-center items-center lg:bg-center min-h-[81vh]  lg:py-0 "
     >
-      <div className="grid grid-cols-1 lg:grid-cols-2 justify-center items-center px-2 sm:px-4 md:px-8 lg:px-16">
-        <div>
+      <div className="grid grid-cols-1 lg:grid-cols-3 justify-center items-center px-2 sm:px-4 md:px-8 lg:px-16">
+        <div className="lg:col-span-2">
           <p className="text-primary py-10 font-bold text-center vie text-[24px] md:text-[30px] lg:text-[36px]">
-            Revolutionizing Mobility for a Sustainable Future
+            Revolutionizing Mobility for a <br /> Sustainable Future
           </p>
           <p className="text-white text-center text-16px] md:text-[20px] lg:text-[24px]">
             Welcome to Redshift, where innovation meets sustainability to
