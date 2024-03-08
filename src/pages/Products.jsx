9786b12b961ce96da791c8221b7ebd568bf2a694
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-08 14:55:54 +0300

    update product page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import ProductIntro from "../components/product/ProductIntro";
+import Scroll from "../components/Reusable/Scroll";
+import ProductThree from "../components/product/ProductThree";
 
 const Products = () => {
-  return <div className='mt-32'>Products</div>;
+  return (
+    <div className="my-32">
+      <ProductIntro />
+      <div className="mt-44">
+        <Scroll />
+      </div>
+      <ProductThree />
+    </div>
+  );
 };
 
 export default Products;
