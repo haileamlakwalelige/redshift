0e1c045de46118886f5f73844c95963d29aedb88
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-08 11:01:32 +0300

    create single testimonies

---
+++ src/components/Reusable/testimony/Testimony.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import test3 from "../../../assets/test3.png";
+
+
+const Testimony = () => {
+  return <div>
+    <div className="flex justify-center items-center flex-col lg:-mt-80">
+            <img src={test3} alt="" className="pb-2" />
+            <p className="text-primary text-[20px] font-bold">Danait K</p>
+            <p className="text-[16px] font-bold text-[#c03b3b] pb-2">
+              Nairobi, Kenya
+            </p>
+            <p className="text-center text-black text-[14px]">
+              As an agent for Redshift, I've seen the positive response from
+              customers firsthand. The crowd funding model is inclusive and
+              allows people from all walks of life to be part of the green
+              mobility movement. Redshift's commitment to quality and
+              sustainability makes it a brand I'm proud to represent.
+            </p>
+          </div>{" "}
+  </div>;
+};
+
+export default Testimony;
