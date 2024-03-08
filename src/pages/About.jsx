10b26c8a62ac8dc3ae0043405a1301b78e991e75
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-08 11:05:11 +0300

    finalize about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import AboutWhat from "../components/about/AboutWhat";
+import AboutMission from "../components/about/AboutMission";
+import AboutVision from "../components/about/AboutVision";
+import AboutEvolution from "../components/about/AboutEvolution";
+import AboutPlan from "../components/about/AboutPlan";
+import AboutMarket from "../components/about/AboutMarket";
+import AboutBusiness from "../components/about/AboutBusiness";
+import AboutPartners from "../components/about/AboutPartners";
 
 const About = () => {
   return (
-    <div className='mt-32'>About</div>
-  )
-}
+    <div className="mt-32">
+      <AboutWhat />
+      <AboutMission />
+      <AboutVision />
+      <AboutEvolution />
+      <AboutPlan />
+      <AboutMarket />
+      <AboutBusiness />
+      <AboutPartners />
+    </div>
+  );
+};
 
 export default About
