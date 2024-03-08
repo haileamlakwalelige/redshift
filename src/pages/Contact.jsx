4a690e99388e0af4a6f9784ebedcb3e5373468c0
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-08 16:43:42 +0300

    update contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import ContactIntro from "../components/contact/ContactIntro";
+import ContactFormTop from "../components/contact/ContactFormTop";
+import ContactAddress from "../components/contact/ContactAddress";
+import ContactForm from "../components/Reusable/contact/ContactForm";
 
 const Contact = () => {
   return (
-    <div className='mt-32'>Contact</div>
-  )
-}
+    <div className="mt-32">
+      <ContactIntro />
+      <ContactAddress />
+      <ContactFormTop />
+      <ContactForm />
+    </div>
+  );
+};
 
 export default Contact
