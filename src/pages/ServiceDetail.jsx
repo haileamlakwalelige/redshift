4000e32d2fe213e638cf7092e5b88f1cd18534ce
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-11 14:54:53 +0300

    add detail project section

--- src/pages/ServiceDetail.jsx
+++ src/pages/ServiceDetail.jsx
@@ -6,18 +6,24 @@ import DetailView from "../components/service detail/DetailView";
 import DetailSecondIntro from "../components/service detail/DetailSecondIntro";
 import DetailSupply from "../components/service detail/DetailSupply";
 import DetailWhat from "../components/service detail/DetailWhat";
+import DetailConstruction from "../components/service detail/DetailConstruction";
+import DetailKey from "../components/service detail/DetailKey";
+import DetailProject from "../components/service detail/DetaiProject";
+import DetailScroll from "../components/service detail/DetailScroll";
 
 const ServiceDetail = () => {
   return (
-    <div
-      className="mt-32 bg-white"
-    >
+    <div className="mt-32 bg-white">
       <DetailIntro />
       <DetailImage />
       <DetailView />
       <DetailSecondIntro />
       <DetailSupply />
       <DetailWhat />
+      <DetailConstruction />
+      <DetailKey />
+      <DetailProject />
+      <DetailScroll />
     </div>
   );
 };
