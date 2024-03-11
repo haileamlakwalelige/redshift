44cc8f6c35151e372bd53fc3265ea99fcfae0d01
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-11 14:54:42 +0300

    add detail project section

---
+++ src/components/service detail/DetaiProject.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const DetailProject = () => {
+  return (
+    <div className="px-2 sm:px-4 md:px-8 lg:px-16">
+      <div className="flex justify-center items-start flex-col ml-2 py-6">
+        <p className="  items-end  font-extrabold vie text-secondary text-[30px] md:text-[35px] lg:text-[40px]">
+          PROJECT SHOWCASE
+        </p>
+        <span className="h-1 -mt-1 lg:-mt-2 ml-3  lg:ml-12 min-w-[152px]  bg-primary"></span>
+      </div>
+      <p className="text-secondary text-[16px] text-start px-2 md:pr-4 lg:pr-8 lg:text-[20px]">
+        View images of our construction projects, showcasing the meticulous
+        planning, skilled craftsmanship, and attention to detail that defines
+        Redshift Corp's construction expertise.
+      </p>
+    </div>
+  );
+};
+
+export default DetailProject;
