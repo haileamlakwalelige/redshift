7053bd5dcab059d04fbf7e95c13f1eaad89a1a69
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-12 11:25:22 +0300

    update blog page

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import BlogIntro from "../components/blog/BlogIntro";
+import BlogNews from "../components/blog/BlogNews";
 
 const Blog = () => {
   return (
     <div className="mt-32">
       <BlogIntro />
+      <BlogNews />
     </div>
   );
 };
