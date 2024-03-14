26c5c836a434d1a8b59bea99b1a3272bf770b9f3
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-14 09:38:23 +0300

    update font-size

--- src/components/home/HomeTestimony.jsx
+++ src/components/home/HomeTestimony.jsx
@@ -7,7 +7,7 @@ const HomeTestimony = () => {
   return (
     <div>
       <div className="flex justify-center items-start flex-col ml-2 py-6">
-        <p className="  items-end max-w-[100px] font-extrabold vie text-secondary text-[25px] sm:text-[30px] md:text-[35px] lg:text-[40px]">
+        <p className="  items-end max-w-[100px] font-extrabold vie text-secondary text-[25px] sm:text-[25px] md:text-[30px] lg:text-[32px] ">
           TESTIMONY
         </p>
         <span className="h-1 -mt-1 lg:-mt-2 ml-3  lg:ml-12 min-w-[222px]  bg-primary"></span>
