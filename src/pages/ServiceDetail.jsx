30b153602e06ed062f3c004a5efe20b94d2250ba
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-14 16:26:44 +0300

    update service detail page

--- src/pages/ServiceDetail.jsx
+++ src/pages/ServiceDetail.jsx
@@ -1,27 +1,26 @@
 import React from "react";
 import DetailIntro from "../components/service detail/DetailIntro";
-import backgroundImage from "../assets/aboutback.png";
 import DetailImage from "../components/service detail/DetailImage";
 import DetailView from "../components/service detail/DetailView";
 import DetailSecondIntro from "../components/service detail/DetailSecondIntro";
 import DetailSupply from "../components/service detail/DetailSupply";
 import DetailWhat from "../components/service detail/DetailWhat";
 import DetailConstruction from "../components/service detail/DetailConstruction";
-import DetailKey from "../components/service detail/DetailKey";
 import DetailProject from "../components/service detail/DetaiProject";
 import DetailScroll from "../components/service detail/DetailScroll";
+import Gallery from "../components/service/Gallery";
 
 const ServiceDetail = () => {
   return (
-    <div className="mt-32 bg-white">
+    <div className="mt-32 bg-white overflow-x-hidden">
       <DetailIntro />
       <DetailImage />
       <DetailView />
       <DetailSecondIntro />
       <DetailSupply />
+      <Gallery />
       <DetailWhat />
       <DetailConstruction />
-      <DetailKey />
       <DetailProject />
       <DetailScroll />
     </div>
