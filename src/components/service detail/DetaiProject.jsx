3123422413171596c6eb5f68a3cbaa34cb5e7764
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-14 16:26:33 +0300

    update detail project

--- src/components/service detail/DetaiProject.jsx
+++ src/components/service detail/DetaiProject.jsx
@@ -4,12 +4,12 @@ const DetailProject = () => {
   return (
     <div className="px-2 sm:px-4 md:px-8 lg:px-16">
       <div className="flex justify-center items-start flex-col ml-2 py-6">
-        <p className="  items-end  font-extrabold vie text-secondary text-[25px] sm:text-[25px] md:text-[30px] lg:text-[32px] ">
+        <p className="  items-end  font-extrabold vie text-secondary text-[18px] lg:text-[20px] pb-6">
           PROJECT SHOWCASE
         </p>
         <span className="h-1 -mt-1 lg:-mt-2 ml-3  lg:ml-12 min-w-[152px]  bg-primary"></span>
       </div>
-      <p className="text-secondary text-[16px] text-start px-2 md:pr-4 lg:pr-8 lg:text-[20px]">
+      <p className="text-secondary text-[16px] text-start px-2 md:pr-4 lg:pr-8 lg:text-[18px]">
         View images of our construction projects, showcasing the meticulous
         planning, skilled craftsmanship, and attention to detail that defines
         Redshift Corp's construction expertise.
