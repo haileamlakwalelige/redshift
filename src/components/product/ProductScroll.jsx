05061ca75f0228342ee6c4b29f39d3eae0c423ce
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-18 00:50:35 +0300

    update product scroll

--- src/components/product/ProductScroll.jsx
+++ src/components/product/ProductScroll.jsx
@@ -3,28 +3,37 @@ import ser1 from "../../assets/ser1.png";
 import ser2 from "../../assets/ser2.png";
 import ser3 from "../../assets/car1.png";
 import ser4 from "../../assets/car2.png";
+import ser5 from "../../assets/ser1.png";
+import ser6 from "../../assets/ser2.png";
+import ser7 from "../../assets/car1.png";
+import ser8 from "../../assets/car2.png";
+import ProductFourTwo from "./ProductFourTwo";
+import ProductThree from "./ProductThree";
 
-function ProductScroll() {
+
+
+function ProductScroll({ onImageSelect }) {
   const images = [
-    { src: ser1, data: "Data for image 1" },
-    { src: ser2, data: "Data for image 2" },
-    { src: ser3, data: "Data for image 3" },
-    { src: ser4, data: "Data for image 4" },
-    { src: ser1, data: "Data for image 1" },
-    { src: ser2, data: "Data for image 2" },
-    { src: ser3, data: "Data for image 3" },
-    { src: ser4, data: "Data for image 4" },
+    { src: ser1, data: "Data for image 1", id: 1 },
+    { src: ser2, data: "Data for image 2", id: 2 },
+    { src: ser3, data: "Data for image 3", id: 3 },
+    { src: ser4, data: "Data for image 4", id: 4 },
+    { src: ser5, data: "Data for image 1", id: 5 },
+    { src: ser6, data: "Data for image 2", id: 6 },
+    { src: ser7, data: "Data for image 3", id: 7 },
+    { src: ser8, data: "Data for image 4", id: 8 },
   ];
 
-  const [selectedImage, setSelectedImage] = useState(images[0].src);
+  const [selectedImage, setSelectedImage] = useState(images[0]);
 
-  const handleImageClick = (src) => {
-    setSelectedImage(src);
+  const handleImageClick = (image) => {
+    setSelectedImage(image);
+    onImageSelect(image.data); // Call the onImageSelect prop and pass the selected image data
   };
 
   useEffect(() => {
-    setSelectedImage(images[0].src);
-  }, []); 
+    setSelectedImage(images[0]);
+  }, []);
 
   return (
     <div
@@ -33,16 +42,14 @@ function ProductScroll() {
     >
       <div className="flex-col lg:flex-row h-70vh gap-4 md:gap-10 lg:gap-20 xl:gap-28 rounded-lg p-4 flex ">
         <div className="lg:h-[80vh] gap-4 carousel carousel-center lg:carousel-vertical rounded-box">
-          {images.map((image, index) => (
-            <div key={index} className="carousel-item">
+          {images.map((image) => (
+            <div key={image.id} className="carousel-item">
               <img
                 src={image.src}
-                alt={`Image ${index + 1}`}
-                onClick={() => handleImageClick(image.src)}
+                alt={`Image ${image.id}`}
+                onClick={() => handleImageClick(image)}
                 className={`carousel-item block mb-4 cursor-pointer h-[203px] w-[288px] ${
-                  selectedImage === image.src
-                    ? " rounded-xl"
-                    : ""
+                  selectedImage === image ? " rounded-xl" : ""
                 }`}
               />
             </div>
@@ -51,13 +58,18 @@ function ProductScroll() {
         <div className="data-display">
           {selectedImage && (
             <img
-              src={selectedImage}
+              src={selectedImage.src}
               alt="Image Data"
               className="h-full w-[60vw]"
             />
           )}
         </div>
       </div>
+      {selectedImage && (
+        <div>
+          {selectedImage.id % 2 === 0 ? <ProductFourTwo /> : <ProductThree />}
+        </div>
+      )}
     </div>
   );
 }
