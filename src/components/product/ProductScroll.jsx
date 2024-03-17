e3df85eb3026d0276c0b2c793b13fe9db045b6b8
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-18 00:10:47 +0300

    update product sections

---
+++ src/components/product/ProductScroll.jsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import ser1 from "../../assets/ser1.png";
+import ser2 from "../../assets/ser2.png";
+import ser3 from "../../assets/car1.png";
+import ser4 from "../../assets/car2.png";
+
+function ProductScroll() {
+  const images = [
+    { src: ser1, data: "Data for image 1" },
+    { src: ser2, data: "Data for image 2" },
+    { src: ser3, data: "Data for image 3" },
+    { src: ser4, data: "Data for image 4" },
+    { src: ser1, data: "Data for image 1" },
+    { src: ser2, data: "Data for image 2" },
+    { src: ser3, data: "Data for image 3" },
+    { src: ser4, data: "Data for image 4" },
+  ];
+
+  const [selectedImage, setSelectedImage] = useState(images[0].src);
+
+  const handleImageClick = (src) => {
+    setSelectedImage(src);
+  };
+
+  useEffect(() => {
+    setSelectedImage(images[0].src);
+  }, []); 
+
+  return (
+    <div
+      data-aos="slide-up"
+      className="px-2 sm:px-6 md:px-12 lg:px-20 xl:px-28"
+    >
+      <div className="flex-col lg:flex-row h-70vh gap-4 md:gap-10 lg:gap-20 xl:gap-28 rounded-lg p-4 flex ">
+        <div className="lg:h-[80vh] gap-4 carousel carousel-center lg:carousel-vertical rounded-box">
+          {images.map((image, index) => (
+            <div key={index} className="carousel-item">
+              <img
+                src={image.src}
+                alt={`Image ${index + 1}`}
+                onClick={() => handleImageClick(image.src)}
+                className={`carousel-item block mb-4 cursor-pointer h-[203px] w-[288px] ${
+                  selectedImage === image.src
+                    ? " rounded-xl"
+                    : ""
+                }`}
+              />
+            </div>
+          ))}
+        </div>
+        <div className="data-display">
+          {selectedImage && (
+            <img
+              src={selectedImage}
+              alt="Image Data"
+              className="h-full w-[60vw]"
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductScroll;
