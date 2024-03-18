de88c2e6e4c31706707acc07892505143b91b898
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-18 17:14:32 +0300

    add reusable component for buttons

---
+++ src/components/Reusable/button/Button.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./button.css";
+
+const Button = ({ title, type }) => {
+  return (
+    <div>
+      <div className="container py-10">
+        <button type={type} className="button text-primary button-3">
+          {title}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Button;
