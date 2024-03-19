9ddb2fbffa76bab00eddfc4d1414c29bd97f3c42
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-19 17:35:35 +0300

    update font-size

--- src/components/home/HomeProducts.jsx
+++ src/components/home/HomeProducts.jsx
@@ -4,10 +4,10 @@ const HomeProducts = () => {
   return (
     <div data-aos="slide-up" className="">
       <div className="flex justify-center items-start flex-col ml-2 py-6">
-        <p className="  items-end max-w-[100px] font-extrabold vie text-secondary text-[18px] lg:text-[20px] pb-2">
+        <p className="  items-end max-w-[100px] font-extrabold vie text-secondary text-[25px] md:text-[30px] lg:text-[32px] pb-2">
           PRODUCTS
         </p>
-        <span className="h-1 -mt-1 lg:-mt-2 ml-3  lg:ml-8 min-w-[152px]  bg-primary"></span>
+        <span className="h-1 -mt-1 lg:-mt-2 ml-3  lg:ml-8 min-w-[152px]  bg-sub"></span>
       </div>
     </div>
   );
