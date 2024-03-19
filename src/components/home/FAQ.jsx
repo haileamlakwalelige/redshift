99e12b1b3eed5809ab7c0b40c36594934a5938c1
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-19 17:34:32 +0300

    update title size

--- src/components/home/FAQ.jsx
+++ src/components/home/FAQ.jsx
@@ -7,7 +7,7 @@ const FAQ = () => {
   return (
     <div style={{ backgroundImage: `url(${bg})` }}>
       <div className="flex justify-center items-start flex-col ml-2 py-6">
-        <p className="  items-end max-w-[100px] font-extrabold vie text-secondary text-[18px] lg:text-[20px] pb-2">
+      <p className="  items-end max-w-[100px] font-extrabold vie text-secondary text-[25px] md:text-[30px] lg:text-[32px] pb-2">
           FAQ
         </p>
         <span className="h-1 -mt-1 lg:-mt-2 ml-3 min-w-[80px]  bg-primary"></span>
