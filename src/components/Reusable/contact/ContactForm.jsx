b0e904bf85673701a4af112ec4187b91144563de
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-21 10:22:23 +0300

    update the contact form

--- src/components/Reusable/contact/ContactForm.jsx
+++ src/components/Reusable/contact/ContactForm.jsx
@@ -1,46 +1,107 @@
-import React from "react";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Button from "../button/Button";
 
 const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    title: "",
+    phone: "",
+    email: "",
+    description: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        "http://192.168.100.29:8000/api/rigistrations/",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+      if (response.ok) {
+        toast.success("Register successfully!");
+        setFormData({
+          title: "",
+          phone: "",
+          email: "",
+          description: "",
+        });
+      } else {
+        toast.error("Failed to submit data");
+      }
+    } catch (error) {
+      console.error("Error submitting data:", error);
+      toast.error("Error submitting data");
+    }
+  };
+
   return (
     <div id="form" className="flex justify-center items-center">
-      <form className="flex flex-col justify-center items-center">
+      <form
+        className="flex flex-col justify-center items-center"
+        onSubmit={handleSubmit}
+      >
         <div className="my-3">
-          <p className="text-secondary text-start text-[24px]   cri font-light">
+          <p className="text-secondary text-start text-[24px] cri font-light">
             Full Name
           </p>
           <input
             type="text"
-            className="bg-faq text-secondary min-w-[300px]  outline-none focus:outline-none placeholder:pl-2 pl-2 lg:min-w-[400px] h-[44px] rounded"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+            className="bg-faq text-secondary min-w-[300px] outline-none focus:outline-none placeholder:pl-2 pl-2 lg:min-w-[400px] h-[44px] rounded"
           />
         </div>
         <div className="my-3">
-          <p className="text-secondary text-start text-[24px]   cri font-light">
+          <p className="text-secondary text-start text-[24px] cri font-light">
             Phone
           </p>
           <input
             type="text"
-            className="bg-faq text-secondary min-w-[300px]  outline-none focus:outline-none placeholder:pl-2 pl-2 lg:min-w-[400px] h-[44px] rounded"
+            name="phone"
+            value={formData.phone}
+            onChange={handleChange}
+            className="bg-faq text-secondary min-w-[300px] outline-none focus:outline-none placeholder:pl-2 pl-2 lg:min-w-[400px] h-[44px] rounded"
           />
         </div>
         <div className="my-3">
-          <p className="text-secondary text-start text-[24px]   cri font-light">
+          <p className="text-secondary text-start text-[24px] cri font-light">
             Email
           </p>
           <input
             type="email"
-            className="bg-faq text-secondary min-w-[300px]  outline-none focus:outline-none placeholder:pl-2 pl-2 lg:min-w-[400px] h-[44px] rounded"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            className="bg-faq text-secondary min-w-[300px] outline-none focus:outline-none placeholder:pl-2 pl-2 lg:min-w-[400px] h-[44px] rounded"
           />
         </div>
         <div className="my-3">
-          <p className="text-secondary text-start text-[24px]   cri font-light">
+          <p className="text-secondary text-start text-[24px] cri font-light">
             Message
           </p>
           <textarea
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
             cols={30}
             rows={4}
-            type="text"
-            className="bg-faq text-secondary min-w-[300px]  outline-none focus:outline-none placeholder:pl-2 pl-2 lg:min-w-[400px] rounded"
+            className="bg-faq text-secondary min-w-[300px] outline-none focus:outline-none placeholder:pl-2 pl-2 lg:min-w-[400px] rounded"
           />
         </div>
         <div className="mt-6">
