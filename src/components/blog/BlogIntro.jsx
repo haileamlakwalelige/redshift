6d2c3d970c99797b5071eb3760d336366ec85301
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-25 09:33:54 +0300

    update blog intro section

--- src/components/blog/BlogIntro.jsx
+++ src/components/blog/BlogIntro.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 // import Wave from "react-wavify";
-import footer from "../../assets/blogbg.png";
+import footer from "../../assets/group.png";
 
 const BlogIntro = () => {
   const [isHovered, setIsHovered] = useState(false);
@@ -24,37 +24,25 @@ const BlogIntro = () => {
       }}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
-      className="min-h-[83vh] blog"
+      className="min-h-screen"
     >
-      <div className="min-h-[85vh] bg flex flex-col items-start justify-center">
-        <div className="md:w-full">
-          <div className=" px-2 sm:px-4 md:px-6 lg:px-10 py-4 ">
-            <p className="text-white text-center font-bold text-[26px] md:text-[29px] lg:text-[40px] xl:text-[50px] pt-4 pb-2 inter">
-              Blog
-            </p>
-            <p className="text-white text-center font-normal pb-2 mt-10 px-[2%] sm:px-[10%] md:px-[15%] lg:px-[25%] text-lg lg:text-xl">
-              Explore our diverse collection of blogs where we delve into
-              various topics related to our mission, vision, and the industries
-              we operate in. Red Shift is not just a manufacturer; we are a
-              source of valuable information, insights, and updates.{" "}
-            </p>
+      <div>
+        <div className="min-h-screen flex flex-col items-start justify-center">
+          <div className="md:w-full">
+            <div className=" px-2 sm:px-4 md:px-6 lg:px-10 py-4 ">
+              <p className="text-white text-center font-bold text-[26px] md:text-[29px] lg:text-[40px] xl:text-[50px] pt-4 pb-2 inter">
+                Blog
+              </p>
+              <p className="text-white text-center font-normal pb-2 mt-10 px-[2%] sm:px-[10%] md:px-[15%] lg:px-[25%] text-lg lg:text-xl">
+                Explore our diverse collection of blogs where we delve into
+                various topics related to our mission, vision, and the
+                industries we operate in. Red Shift is not just a manufacturer;
+                we are a source of valuable information, insights, and updates.{" "}
+              </p>
+            </div>
           </div>
         </div>
       </div>
-
-      {/* <div>
-        <Wave
-          fill="#45C3FE"
-          paused={isHovered ? false : true}
-          options={{
-            height: 40,
-            amplitude: 40,
-            speed: 0.3,
-            points: 2,
-          }}
-          className="bottom-0 -mb-2 z-10 -mt-8 rounded-b-2xl relative"
-        />
-      </div> */}
     </div>
   );
 };
