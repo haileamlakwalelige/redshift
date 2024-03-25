bf85e63b4494f75e6ab7f1c4a97a617468298c08
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-25 13:07:58 +0300

    update the button

--- src/components/Reusable/button/Button.jsx
+++ src/components/Reusable/button/Button.jsx
@@ -4,8 +4,11 @@ import "./button.css";
 const Button = ({ title, type }) => {
   return (
     <div>
-      <div className="container py-10">
-        <button type={type} className="button text-primary button-3 flex justify-center items-center text-center">
+      <div className="container py-10 justify-center items-center">
+        <button
+          type={type}
+          className="button text-primary button-3 flex justify-center items-center text-center"
+        >
           {title}
         </button>
       </div>
