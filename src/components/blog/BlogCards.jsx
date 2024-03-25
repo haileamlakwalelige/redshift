4a69baa294522ec1c3d283b94df2c1cb426ded07
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-25 09:11:40 +0300

    update blog cards

--- src/components/blog/BlogCards.jsx
+++ src/components/blog/BlogCards.jsx
@@ -39,15 +39,15 @@ const BlogCards = () => {
   };
 
   return (
-    <div className="mt-16">
-      <div className="carousel gap-10 lg:gap-16 px-2 sm:px-4 md:px-8 lg:px-16 mx-2 sm:mx-4 md:mx-8">
+    <div className="mt-16 px-10">
+      <div className="carousel gap-10 lg:gap-16 sm:px-4 md:px-8 lg:px-16 mx-2 sm:mx-4 md:mx-8 px-10">
         {blogs.map((item, index) => (
-          <div className="carousel-item card" key={item.id}>
+          <div className="carousel-item" key={item.id}>
             {item.title && item.content && item.date && (
               <div
                 onMouseEnter={() => handleMouseEnter(index)}
                 onMouseLeave={() => handleMouseLeave(index)}
-                className="section bg-white max-w-[280px] lg:max-w-[320px] min-h-[410px] md:min-h-[416px] lg:min-h-[345px] lg:max-h-[400px] rounded-xl border-[1px] z-20 overflow-y-hidden shadow-xl"
+                className="section bg-white max-w-[280px] min-h-[410px] md:min-h-[416px] lg:min-h-[345px] lg:max-h-[400px] rounded-xl border-[1px] z-20 overflow-y-hidden shadow-xl"
               >
                 <p className="text-primary line-clamp-1 my-6 px-2 inter border-b-[2px] border-primary font-bold text-[16px] md:text-[18px] lg:text-[20px] text-center py-2">
                   {item.title}
@@ -62,20 +62,10 @@ const BlogCards = () => {
                   </p>
                 </div>
                 <div className="flex flex-col items-center">
-                  <Wave
-                    fill="#36A0D2"
-                    paused={!hoverStates[index]}
-                    options={{
-                      height: 60,
-                      amplitude: 40,
-                      speed: 0.3,
-                      points: 2,
-                    }}
-                    className="bottom-0 -mb-2 z-10 -mt-8 rounded-b-2xl "
-                  />
+                 
                   <Link
                     to={`/blog/${encodeURIComponent(item.id)}`}
-                    className="-mt-24 text-primary font-bold text-[16px] md:text-[18px] lg:text-[20px] pt-14 z-20"
+                    className=" text-primary font-bold text-[16px] md:text-[18px] lg:text-[20px] pt-14 z-20"
                   >
                     <p>Read More ...</p>
                   </Link>
