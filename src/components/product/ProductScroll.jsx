9d4b506186bee9cc6ae281296157dec26c641e5e
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-27 09:31:09 +0300

    add animation duration and change scrolling images

--- src/components/product/ProductScroll.jsx
+++ src/components/product/ProductScroll.jsx
@@ -40,14 +40,14 @@ function ProductScroll({ onImageSelect }) {
   }, []);
 
   return (
-    <div data-aos="slide-up" data-aos-duration="1500" className="px-2 sm:px-6">
+    <div data-aos="slide-up" data-aos-duration="3000" className="px-2 sm:px-6">
       <div className="flex-col lg:flex-row h-70vh gap-4 md:gap-10 lg:gap-20 xl:gap-28 rounded-lg p-4 flex px-2 sm:px-6 md:px-12 lg:px-20 xl:px-28">
         <div className="lg:h-[80vh] gap-4 carousel carousel-center lg:carousel-vertical rounded-box">
           {images.map((image, index) => (
             <div key={index} className="carousel-item">
               <img
                 src={image.src}
-                alt={`Image ${index+1}`}
+                alt={`Image ${index + 1}`}
                 onClick={() => handleImageClick(image)}
                 className={`carousel-item block mb-4 cursor-pointer h-[203px] w-[288px] ${
                   selectedImage === image ? " rounded-xl" : ""
@@ -61,7 +61,7 @@ function ProductScroll({ onImageSelect }) {
             <img
               src={selectedImage.src}
               alt="Image Data"
-              className="h-full w-full  lg:w-[60vw]"
+              className="h-full w-full  lg:w-[60vw] max-h-[80vh]"
             />
           )}
         </div>
