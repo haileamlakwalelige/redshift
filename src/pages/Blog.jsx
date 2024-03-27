9ad3633683948a2c7c3f60b9ee9627cf8859e83c
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-27 10:44:32 +0300

    update the page

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import BlogIntro from "../components/blog/BlogIntro";
-import BlogNews from "../components/blog/BlogNews";
 import BlogCards from "../components/blog/BlogCards";
 
 const Blog = () => {
   return (
     <div className=" overflow-x-hidden">
       <BlogIntro />
-      {/* <BlogNews /> */}
       <BlogCards />
     </div>
   );
